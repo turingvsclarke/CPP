dbed63c2e42e464600dd41a96998c739d270afeb
Eades <ryeades@IN-ULIB-435-I1.local>
2022-09-16 17:53:34 -0400

    Arithmetic operations from 09.16 added

---
+++ incDec.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+	int x=5;
+	std::cout<<"Initial value: "<<x<<std::endl;
+	std::cout<<"Post increment: "<<x++<<std::endl;
+	std::cout<<"Pre increment: "<<++x<<std::endl;
+
+} // end main
