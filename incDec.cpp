e71785c4458960ee869bf866a600772e4323cd02
turingvsclarke <rhinoeades@gmail.com>
2022-10-13 19:29:41 -0400

    Cleaning up directory

--- incDec.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main(){
-	int x=5;
-	std::cout<<"Initial value: "<<x<<std::endl;
-	std::cout<<"Post increment: "<<x++<<std::endl;
-	std::cout<<"Pre increment: "<<++x<<std::endl;
-
-} // end main
