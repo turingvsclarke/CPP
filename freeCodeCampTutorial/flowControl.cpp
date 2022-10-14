6d80f73793cb81cd600b823bb7b3b20a4959d7de
turingvsclarke <40091844+turingvsclarke@users.noreply.github.com>
2022-10-13 20:37:07 -0400

    Create flowControl.cpp

---
+++ freeCodeCampTutorial/flowControl.cpp
@@ -0,0 +1,29 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+  int tool=8;
+  switch(tool){
+    
+    case 8: {
+        std::cout<<"Active tool is pen"<<std::endl;
+        }  
+    break;
+        
+    case 4: {
+        std::cout<<"Active tool is marker"<<std::endl;
+        }
+    break;
+    
+    default: {
+        std::cout<<"Can't match any tool"<<std::endl;
+        }
+    }
+    
+    int x=(5>3)?5:3;
+    std::cout<<x;
+    
+    
+}
