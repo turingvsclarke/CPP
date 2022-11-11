3e5ecb119a7542730287b0bc4029b6cb388f7470
turingvsclarke <rhinoeades@gmail.com>
2022-11-11 16:59:05 -0500

    char/string manipulation section finished

--- freeCodeCampTutorial/cstring.cpp
+++ freeCodeCampTutorial/cstring.cpp
@@ -16,4 +16,30 @@ int main(){
 	int n=10;
 	/// strncmp stops trying to compare after n chars
 	std::cout<<"strncmp ("<<string<<","<<string2<<","<<n<<") : "<<strncmp(string,string2,n)<<std::endl;
+
+
+	/*********   Finding all the instances of a given character ***********/
+	char str[]="This is a silly, snaking string with a lot of s's";
+
+	std::cout<<"Looking for the 's' character in: \n"<<str<<std::endl;
+	char* result=str;
+	int instances=0;
+	while((result=strchr(result,'s'))!=nullptr){
+		result++;
+		instances++;
+		
+	}
+	
+	std::cout<<"We found "<<instances<<" instances of the character"<<std::endl;
+
+	/*** Finding the last instance of a character in a string ****/\
+	char path[]="/CPP/freeCodeCampTutorial/cstring.cpp";
+	char* output=strrchr(path,'/');
+
+	std::cout<<"The file path is:\n"<<path<<std::endl;
+	if(output){
+		std::cout<<"The file name is "<<output+1<<std::endl;
+	}
+
+
 } // end main
