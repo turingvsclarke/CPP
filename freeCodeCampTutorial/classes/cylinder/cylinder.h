8cb29282142f7923dc36e3e8d3a5c640da8b96d6
turingvsclarke <40091844+turingvsclarke@users.noreply.github.com>
2022-12-12 16:03:26 -0500

    Create cylinder.h

---
+++ freeCodeCampTutorial/classes/cylinder/cylinder.h
@@ -0,0 +1,30 @@
+// cylinder.h
+#ifndef  CYLINDER_H
+#define  CYLINDER_H
+#include "constants.h"
+
+class Cylinder {
+    // public: 
+    private:
+        double radius=1;
+        double height=1; 
+        
+    public:
+        // This forces the compiler to make a default, null-param constructor
+        Cylinder()=default;
+        /***
+        Cylinder(){
+            radius=2;
+            height=2;
+        }
+        ***/
+        Cylinder(double rad, double h);
+        double volume();
+ 
+        double getRadius(); 
+        double getHeight();
+        void setRadius(double rad);
+        void setHeight(double h); 
+        
+}; // end class
+#endif 
