3f635ff02aef47e5744286c39b8dc07f9d0369b4
turingvsclarke <40091844+turingvsclarke@users.noreply.github.com>
2022-12-12 16:02:53 -0500

    Create cylinder.cpp

---
+++ freeCodeCampTutorial/classes/cylinder/cylinder.cpp
@@ -0,0 +1,24 @@
+// cylinder.cpp
+#include "cylinder.h"
+
+// This forces the compiler to make a default, null-param constructor
+Cylinder::Cylinder(double rad, double h){
+    radius=rad;
+    height=h;
+}
+double Cylinder::volume(){
+    return radius*radius*PI*height;    
+}
+ 
+double Cylinder::getRadius(){
+    return radius;    
+}
+double Cylinder::getHeight(){
+    return height;    
+}
+void Cylinder::setRadius(double rad){
+    radius=rad;
+}
+void Cylinder::setHeight(double h){
+    height=h;    
+}
