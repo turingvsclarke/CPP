3ee2193aff80fbef4c147967ba44c43f2ab2e8b5
turingvsclarke <40091844+turingvsclarke@users.noreply.github.com>
2022-12-12 16:04:01 -0500

    Create main.cpp

---
+++ freeCodeCampTutorial/classes/cylinder/main.cpp
@@ -0,0 +1,22 @@
+// main.cpp
+
+#include <iostream>
+#include <string>
+#include "constants.h"
+#include "cylinder.cpp"
+
+int main()
+{
+    // With a parameter constructor, the compiler no longer generates a default empty one
+    Cylinder cylinder1;
+    std::cout<<"Volume: "<<cylinder1.volume()<<std::endl;
+    /***
+    cylinder1.radius=10;
+    cylinder1.height=3;
+    ***/
+    Cylinder cylinder2(5,6);
+    std::cout<<"Volume: "<<cylinder2.volume()<<std::endl;
+    cylinder2.setRadius(10);
+    cylinder2.setHeight(3);
+    std::cout<<"Volume: "<<cylinder2.volume()<<std::endl;
+}
