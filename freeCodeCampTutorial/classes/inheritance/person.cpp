ef953dd6c70475f9625374be05b6ff2596c43f0d
turingvsclarke <40091844+turingvsclarke@users.noreply.github.com>
2022-12-18 15:07:00 -0500

    Update person.cpp

--- freeCodeCampTutorial/classes/inheritance/person.cpp
+++ freeCodeCampTutorial/classes/inheritance/person.cpp
@@ -1,1 +1,14 @@
-
+#include<string>
+class Person{
+  public:
+    std::string f_name;
+    std::string l_name;
+    int getAge(){
+        return age;
+    }
+    void setAge(int age){
+        this->age=age;
+    }
+  private:
+    int age;
+};
