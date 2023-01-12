32217dd4602ddb2618381e49ddba5d8b879487f1
turingvsclarke <40091844+turingvsclarke@users.noreply.github.com>
2023-01-12 16:57:10 -0500

    Create person.h

---
+++ freeCodeCampTutorial/classes/inheritance/person.h
@@ -0,0 +1,45 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include<string>
+#include<iostream>
+
+
+class Person{
+  // This function is allegedly going to let us print out class data really easily
+  friend std::ostream& operator<<(std::ostream& out, const Person& person);
+  public:
+    Person();
+    Person(std::string fname, std::string lname);
+    ~Person();
+    
+    int getAge(){
+        return age;
+    }
+    std::string getLname() const{
+        return lname;
+    }
+    std::string getFname() const{
+        return this->fname;
+    }
+    
+    void setFname(std::string fname){
+        this->fname=fname;
+    }
+    
+    void setLname(std::string lname){
+        this->lname=lname;
+    }
+    
+    void setAge(int age){
+        this->age=age;
+    }
+    
+    
+  private:
+    std::string fname="Pseudonymous";
+    std::string lname="Bosch";
+    int age=5;
+};
+
+#endif
