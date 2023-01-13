7e4913f78ea8c0a66b7e614db639a2aec77154f7
turingvsclarke <40091844+turingvsclarke@users.noreply.github.com>
2023-01-13 17:28:23 -0500

    Update player.cpp

--- freeCodeCampTutorial/classes/inheritance/player.cpp
+++ freeCodeCampTutorial/classes/inheritance/player.cpp
@@ -1,12 +1,17 @@
 #include "player.h"
 
 std::ostream& operator<<(std::ostream& out, const Player& player){
-     out<<"Player : [ game : "<<player.game<<" names : "<<player.getFname()<<" "<<player.getLname()<<"]";   
-    //out<<"Player : [ game : "<<player.game<<" names : "<<player.fname()<<" "<<player.lname()<<"]"; // This doesn't work because fname is private in base class
+    out<<"Player : [ game : "<<player.game<<" names : "<<player.getFname()<<" "<<player.getLname()<<"]";   
+    //out<<"Player : [ game : "<<player.game<<" names : "<<player.fname<<" "<<player.lname<<"]"; // This only works if fname/lname are protected/public
     return out;
 }
 
-Player::Player(std::string_view game){
+Player::Player(std::string_view game,std::string_view fname,std::string_view lname){
     this->game=game;
+    this->setFname(fname);
+    this->setLname(lname);
+    this->fname=fname;  
+    this->lname=lname;
+    // this only works if fname is protected/public
 }
 Player::~Player(){};
