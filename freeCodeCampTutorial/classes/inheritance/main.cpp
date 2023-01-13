8624803e701b15af43af11fe9144f349f3e36061
turingvsclarke <40091844+turingvsclarke@users.noreply.github.com>
2023-01-13 17:28:37 -0500

    Update main.cpp

--- freeCodeCampTutorial/classes/inheritance/main.cpp
+++ freeCodeCampTutorial/classes/inheritance/main.cpp
@@ -5,7 +5,9 @@ int main()
 {
     Person human=Person("John","Cena");
     std::cout<<human<<std::endl;
-    Player p1=Player("Fortnite");
+    Player p1=Player("Fortnite","Dan","Williams");
     p1.setFname("Tom");
+    
+    // p1.fname="John"; // This only works if fname is public, not private/protected
     std::cout<<p1;
 }
